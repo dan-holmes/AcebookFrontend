be8596cb6c6873e564c61972bc222d0eeaa9f72b
Dan Holmes <dh@danielholmes.com>
2020-11-24 15:32:32 +0000

    Add styling to posts.

--- src/Post.js
+++ src/Post.js
@@ -4,9 +4,12 @@ class Post extends React.Component {
   render() {
     return (
       <div className="post">
-        {this.props.data.message}<br />
-        {this.props.data.username}<br />
-        {this.props.data.created_at}<br /><br />
+        <div className="post-message">
+          {this.props.data.username}: {this.props.data.message}
+        </div>
+        <div className="post-created-at">
+          {this.props.data.created_at}
+        </div>
       </div>
     )
   }
